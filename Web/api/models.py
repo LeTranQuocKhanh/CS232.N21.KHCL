d97bb40894733e52cb64ae00842a809d6c49efe6
duongve13112002 <71595470+duongve13112002@users.noreply.github.com>
2023-03-12 21:14:32 +0700

    Update models.py

--- Web/api/models.py
+++ Web/api/models.py
@@ -1,1 +1,5 @@
+from django.db import models
 
+class Document(models.Model):
+    document = models.FileField(upload_to='documents/%Y/%m/%d/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
